88114ac398131bb2ed58b96d5456c610827d8491
UrbanoFonseca <francisco.fonseca@ydata.ai>
2021-06-29 23:37:13 +0100

    chore(repo): updated basic repo structure

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# YData's open-source-template
+# ydata-quality
+YData open-source lib for Data Quality.
 
-New YData Open Source repositories should use this repository as a template.
+### venv
+_How to create a virtual environment to run the ydata_quality package._
+
+```bash
+# create the virtual environment
+$ python -m venv .venv
+
+# activate the environment
+$ . ./.venv/bin/activate
+
+# build the package
+$ make package
+
+# install from package
+$ make install
+```
---
+++ setup.py
@@ -0,0 +1,50 @@
+from fileinput import FileInput
+from setuptools import setup, find_namespace_packages
+from pathlib import Path
+
+
+here = Path(__file__).parent.resolve()
+
+requirements = (here / "requirements.txt").read_text(encoding="utf8")
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+version = (here / 'VERSION').read_text().rstrip("\n")
+
+with open('src/ydata_quality/version.py', 'w') as version_file:
+  version_file.write(f'__version__ = \'{version}\'')
+
+setup(name='ydata-quality',
+      version=version,
+      description='YData open-source tools for Data Quality.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='YData',
+      author_email='community@ydata.ai',
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      keywords='data science ydata',
+      url='https://github.com/ydataai/ydata-quality',
+      license="https://github.com/ydataai/ydata-quality/blob/master/LICENSE",
+      python_requires=">=3.6, <3.9",
+      packages=find_namespace_packages('src'),
+      package_dir={'':'src'},
+      include_package_data=True,
+      options={"bdist_wheel": {"universal": True}},
+      install_requires=requirements)
