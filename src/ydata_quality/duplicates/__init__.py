c9de23c7c28fe3e124ab3cc558a98379346a7468
UrbanoFonseca <francisco.fonseca@ydata.ai>
2021-06-30 00:00:56 +0100

    feat(duplicates): added duplicates mvp

---
+++ src/ydata_quality/duplicates/__init__.py
@@ -0,0 +1,9 @@
+"""
+Tools to guarantee data quality on duplicate records.
+"""
+
+from ydata_quality.duplicates.engine import DuplicateChecker
+
+__all__ = [
+    "DuplicateChecker"
+]
---
+++ src/ydata_quality/duplicates/engine.py
@@ -0,0 +1,106 @@
+"""
+Implementation of DuplicateChecker engine class to run duplicate records analysis.
+"""
+
+from typing import List, Optional
+
+import pandas as pd
+from ydata_quality.core import QualityWarning, QualityEngine
+
+class DuplicateChecker(QualityEngine):
+    "Engine for running analyis on duplicate records."
+
+    def __init__(self, df: pd.DataFrame, entities: List[str] = []):
+        self._df = df
+        self._entities = entities
+        self._warnings = set()
+        self._tests = ["exact_duplicates", "entity_duplicates", "duplicate_columns"]
+
+    @property
+    def entities(self):
+        "Property that returns the entities relevant for duplicates analysis."
+        return self._entities
+
+    @entities.setter
+    def entities(self, entities: List[str]):
+        if not isinstance(entities, list):
+            raise ValueError("Property 'entities' should be a list.")
+        assert all([c in self.df.columns for c in entities]), "Given entities should exist as DataFrame's columns."
+        self._entities = entities
+
+    @staticmethod
+    def __get_duplicates(df: pd.DataFrame):
+        "Returns duplicate records."
+        return df[df.duplicated()]
+
+    @staticmethod
+    def __get_entity_duplicates(df: pd.DataFrame, entity: str):
+        "Returns the duplicate records aggregated by a given entity."
+        return df.groupby(entity).apply(DuplicateChecker.__get_duplicates).reset_index(drop=True)
+
+    def exact_duplicates(self):
+        "Returns a DataFrame filtered for exact duplicate records."
+        dups = self.__get_duplicates(self.df) # Filter for duplicate instances
+        if len(dups) > 0:
+            self._warnings.add(
+                QualityWarning(
+                    test='Exact Duplicates', category='Duplicates', priority=2, data=dups,
+                    description=f"Found {len(dups)} instances with exact duplicate feature values."
+            ))
+        else:
+            print("[EXACT DUPLICATES] No exact duplicates were found.")
+            dups = None
+        return dups
+
+    def entity_duplicates(self, entity: Optional[str] = None):
+        """Returns a dict of (entity_value: duplicates) of duplicate records after grouping by an entity.
+
+        If entity is not specified, compute for all entities defined in the init.
+        TODO: Enable computation of duplicates per multiple entities at once (e.g. groupby(['featureA', 'featureB'])).
+        """
+        # Computation Decision Tree
+        # entity is specified : compute for given entity, return dict {entity_values: duplicate records}
+        # entity is not specified
+        #   -> entities is None : skip the test
+        #   -> entities is not None : defaults to entities defined in the init
+
+        if entity is not None: # entity is specified
+            ent_dups = {}
+            dups = self.__get_entity_duplicates(self.df, entity)
+            if len(dups) > 0:                        # if we have any duplicates
+                self._warnings.add(
+                    QualityWarning(
+                        test='Entity Duplicates', category='Duplicates', priority=2, data=dups,
+                        description=f"Found {len(dups)} duplicates after grouping by entities."
+                ))
+                for val in set(dups[entity]):        # iterate on each entity with duplicates
+                    ent_dups[val] = dups[dups[entity]==val]
+            return ent_dups
+        else: # if entity is not specified
+            if len(self.entities) == 0:
+                print("[ENTITY DUPLICATES] There are no entities defined to run the analysis. Skipping the test.")
+                return None
+            else:
+                return {col: self.entity_duplicates(col) for col in self.entities}
+
+
+    def duplicate_columns(self):
+        "Returns a mapping dictionary of columns with fully duplicated feature values."
+        dups = {}
+        for idx, col in enumerate(self.df.columns): # Iterate through all the columns of dataframe
+            ref = self.df[col]                      # Take the column values as reference.
+            for tgt_col in self.df.columns[idx+1:]: # Iterate through all other columns
+                if ref.equals(self.df[tgt_col]):    # Take target values
+                    dups[col] = tgt_col  # Store if they match
+
+        if len(dups) > 0:
+            self._warnings.add(
+                QualityWarning(
+                    test='Duplicate Columns', category='Duplicates', priority=1, data=dups,
+                    description=f"Found {len(dups)} columns with exactly the same feature values as other columns."
+                )
+            )
+        else:
+            print("[DUPLICATE COLUMNS] No duplicate columns were found.")
+            dups = None
+        return dups
