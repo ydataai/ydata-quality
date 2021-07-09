b30b51fa3899c17bc4c42d48f02f8e0e53ca1754
FranciscoSantos <francisco.santos@ydata.ai>
2021-07-09 14:21:50 +0100

    flatline detection

--- src/ydata_quality/valued_missing_values/engine.py
+++ src/ydata_quality/valued_missing_values/engine.py
@@ -4,6 +4,7 @@ Implementation of Valued Missing Values Identifier engine class to run valued mi
 
 from typing import List, Optional, Union
 
+import numpy as np
 import pandas as pd
 
 from ydata_quality.core import QualityEngine, QualityWarning
@@ -20,10 +21,12 @@ class VMVIdentifier(QualityEngine):
                 Provide only to override default timeseries inference on the dataset index.
         """
         super().__init__(df=df)
-        self._tests = ["odd_dtypes"]
-        self._time_index = time_index  # None implies the index
+        self._tests = ["flatlines", "mixed_dtypes"]
+        self._time_index = time_index  # None implies the dataframe index
         self._is_time_series = self._check_time_index()  # Validates status regarding being a time series or not
-        self._missing_mask = self._get_missing_mask()  # Will help to make some methods more efficient
+        self._flatline_index = {}
+        self._default_VMVs = ["?", "UNK", "Unknown", "N/A", "NA", "", "(blank)"]
+        self.__default_index_name = '__index'
 
     def _check_time_index(self):
         """Tries to infer from current time_index column if the dataframe is a timeseries or not.
@@ -31,14 +34,12 @@ class VMVIdentifier(QualityEngine):
         Raises warning if a passed time_index test fails, will not raise on default index infer.
         Sets _is_time_series to True if the test passes"""
         raises = False if self._time_index is None else True  # If no _time_index is passed we don't raise warning
-        if self._time_index  and self._time_index != '__index':
+        if self._time_index  and self._time_index != self.__default_index_name:
             time_index = self.df[self._time_index]
         else:
             time_index = self.df.index  # Only tests index if no time_index column is passed
         if isinstance(time_index, (pd.DatetimeIndex, pd.PeriodIndex, pd.TimedeltaIndex)):
             return True
-        # Here we could still try a conversion method to pd.DatetimeIndex but avoiding corner 
-        # cases does not seem trivial. Best to have the user provide us with a valid index.
         if raises:
             self._warnings.add(
                 QualityWarning(
@@ -48,27 +49,51 @@ class VMVIdentifier(QualityEngine):
             )
         return False
 
-    def _get_missing_mask(self):
-        """Returns a missing mask for the full dataframe."""
-        return self.df.isna()
+    @staticmethod
+    def __get_flatline_index(column: pd.Series):
+        """Returns an index for flatline events on a passed column.
+        A flatline event is any full sequence of repeated values on the column.
+        The returned index is a compact representation of all occurrences of flatline events.
+        Returns a DataFrame with index equal to the index of first element in the event,
+        a tail column identifying the last element of the sequence and a length column."""
+        column.fillna('__filled')  # NaN values were not being treated as a sequence
+        cum_differs_previous = column.ne(column.shift()).cumsum()
+        sequence_groups = column.index.to_series().groupby(cum_differs_previous)
+        data = {'length': sequence_groups.count().values,
+        'ends': sequence_groups.last().values}
+        return pd.DataFrame(data, index=sequence_groups.first().values).query('lengths > 1')
 
-    def flatlines(self):
-        """Iterates over columns of the dataset looking for flatlines (adjacent duplicate element sequences)"""
-        df = self.df.copy()
-        df.rese
-        for column in self.df.columns
-    
-    def odd_dtypes(self):
-        """Infers dtype for the columns.
-        Based on the infered type will or will not perform element dtype inference for the uniques of the column."""
-        infer = pd.api.types.infer_dtype
-        to_inspect = {}
-        for column in self.df.columns:
-            col_dtype = infer(self.df[column])
-            if "mixed" in col_dtype:
-                to_inspect[column] = col_dtype
-            else:
-                continue
-        for column, dtype in to_inspect:
+    def flatlines(self, th: int=5, skip: list=[]):
+        """Checks the flatline index for flat sequences of length over a given threshold.
+        Raises warning indicating columns with flatline events and total flatline events in the dataframe.
+        Arguments:
+            th: Defines the minimum length required for a flatline event to be reported.
+            skip: List of features that will not be target of search for flatlines.
+                Pass '__index' in skip to skip looking for flatlines at the index."""
+        df = self.df.copy()  # Index will not be covered in column iteration
+        df[self.__default_index_name] = df.index  # Index now in columns to be processed next
+        flatlines = {}
+        for column in df.columns:  # Compile flatline index
+            if column in skip:
+                continue  # Column not requested
+            flt_index = self._flatline_index.setdefault(column,
+                self.__get_flatline_index(df[column]))
+            flts = flt_index.loc[flt_index['length']>th]
+            if len(flts) > 0:
+                flatlines[column] = flts
+        if len(flatlines)>0:  # Flatlines detected
+            total_flatlines = [flts.shape[0] for flts in flatlines.values()]
+            self._warnings.add(
+                QualityWarning(
+                    test='Flatlines', category='Valued Missing Values', priority=2, data=flatlines,
+                    description=f"Found {total_flatlines} flatline events with a minimun length of {th} among the columns {set(flatlines.keys())}."
+            ))
+            return flatlines
+        else:
+            print("[FLATLINES] No flatline events with a minimum length of {} were found.")
 
+    def predefined_valued_missing_values(self):
+        """Runs a check against a list of predefined Valued Missing Values.
+        Raises warning based on the existance of these values and returns a DataFrame\
+        with count distribution for each predefined type over each column"""
         
