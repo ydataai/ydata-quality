eb647bd977e8a57c5f030eaf1c8debbfb482a147
FranciscoSantos <francisco.santos@ydata.ai>
2021-07-11 19:45:42 +0100

    review changes

--- src/ydata_quality/valued_missing_values/engine.py
+++ src/ydata_quality/valued_missing_values/engine.py
@@ -2,9 +2,8 @@
 Implementation of Valued Missing Values Identifier engine class to run valued missing value analysis.
 """
 
-from typing import List, Optional, Union
+from typing import Optional
 
-import numpy as np
 import pandas as pd
 
 from ydata_quality.core import QualityEngine, QualityWarning
@@ -13,21 +12,39 @@ from ydata_quality.core import QualityEngine, QualityWarning
 class VMVIdentifier(QualityEngine):
     "Engine for running analysis on valued missing values."
 
-    def __init__(self, df: pd.DataFrame, VMV_extensions: Optional[list]=[]):
+    def __init__(self, df: pd.DataFrame, vmv_extensions: Optional[list]=[]):
         """
         Args:
             df (pd.DataFrame): DataFrame used to run the missing value analysis.
-            time_index (str, optional): references column to be used as index in timeseries df.
-                Provide only to override default timeseries inference on the dataset index.
         """
-        assert isinstance(VMV_extensions, list), "VMV extensions must be passed as a list"
         super().__init__(df=df)
         self._tests = ["flatlines", "predefined_valued_missing_values"]
+        self._default_vmvs = None
         self._flatline_index = {}
         self.__default_index_name = '__index'
-        self._default_VMVs = set(["?", "UNK", "Unknown", "N/A", "NA", "", "(blank)"])
-        self._VMVs = self._default_VMVs.union(set(VMV_extensions))
-        
+        self.vmvs = vmv_extensions
+    
+    @property
+    def default_vmvs(self):
+        """Returns the default list of Valued Missing Values."""
+        if self._default_vmvs is None:
+            self._default_vmvs = set([vmv.lower() if isinstance(vmv, str) else vmv for vmv in ["?", "UNK", "Unknown", "N/A", "NA", "", "(blank)"]])
+            print(self._default_vmvs)
+        return self._default_vmvs
+
+    @property
+    def vmvs(self):
+        """Returns the extended Value Missing Values (default plus user provided)."""
+        if not  self._vmvs:
+            self._vmvs = self.default_vmvs
+        return self._vmvs
+
+    @vmvs.setter
+    def vmvs(self, vmv_extensions: Optional[list] = []):
+        """Allows extending default Valued Missing Values list, append only."""
+        assert isinstance(vmv_extensions, list), "vmv extensions must be passed as a list"
+        self._vmvs = self.default_vmvs.union(set([vmv.lower() if isinstance(vmv, str) else vmv for vmv in vmv_extensions]))
+
     @staticmethod
     def __get_flatline_index(column: pd.Series):
         """Returns an index for flatline events on a passed column.
@@ -74,31 +91,32 @@ class VMVIdentifier(QualityEngine):
 
     def predefined_valued_missing_values(self, skip: list=[]):
         """Runs a check against a list of predefined Valued Missing Values.
-        Raises warning based on the existance of these values.
+        Raises warning based on the existence of these values.
         Returns a DataFrame with count distribution for each predefined type over each column.
         Arguments:
-            skip: List of columns that will not be target of search for VMVs.
+            skip: List of columns that will not be target of search for vmvs.
                 Pass '__index' in skip to skip looking for flatlines at the index."""
         df = self.df.copy()  # Index will not be covered in column iteration
         df[self.__default_index_name] = df.index  # Index now in columns to be processed
         check_cols = set(df.columns).difference(set(skip))
         df = df[check_cols]
-        VMVs = pd.DataFrame(index=self._VMVs, columns=check_cols)
-        for VMV in self._VMVs:
-            VMVs.loc[VMV] = (df==VMV).sum()
-        no_VMV_cols = VMVs.columns[VMVs.sum()==0]
-        no_VMV_rows = VMVs.index[VMVs.sum(axis=1)==0]
-        VMVs.drop(no_VMV_cols, axis=1, inplace=True)
-        VMVs.drop(no_VMV_rows, inplace=True)
-        if VMVs.empty:
-            print("[PREDEFINED VALUED MISSING VALUES] No predefined VMVs from  the set {} were found in the dataset.".format(
+        vmvs = pd.DataFrame(index=self._vmvs, columns=check_cols)
+        for vmv in self._vmvs:
+            check_vmv = lambda x: x.lower()==vmv if isinstance(x,str) else x==vmv
+            vmvs.loc[vmv] = df.applymap(check_vmv).sum()
+        no_vmv_cols = vmvs.columns[vmvs.sum()==0]
+        no_vmv_rows = vmvs.index[vmvs.sum(axis=1)==0]
+        vmvs.drop(no_vmv_cols, axis=1, inplace=True)
+        vmvs.drop(no_vmv_rows, inplace=True)
+        if vmvs.empty:
+            print("[PREDEFINED VALUED MISSING VALUES] No predefined vmvs from  the set {} were found in the dataset.".format(
                 self.predefined_valued_missing_values
             ))
         else:
-            total_VMVs = VMVs.sum().sum()
+            total_vmvs = vmvs.sum().sum()
             self._warnings.add(
                 QualityWarning(
-                    test='Predefined Valued Missing Values', category='Valued Missing Values', priority=2, data=VMVs,
-                    description=f"Found {total_VMVs} VMVs in the dataset."
+                    test='Predefined Valued Missing Values', category='Valued Missing Values', priority=2, data=vmvs,
+                    description=f"Found {total_vmvs} vmvs in the dataset."
             ))
-            return VMVs
+            return vmvs
